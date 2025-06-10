1a95d7f3a5f5f5aac41ba55cabb18091e377e37d
alb <gagliardialberto.98@gmail.com>
2025-06-10 14:44:11 +0200

    firestore service update

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,8 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/character_model.dart';
 import '../models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final characterCollection = FirebaseFirestore.instance.collection('characters');
+
+
+  Future<List<CharacterModel>> fetchCharacters() async {
+    final snapshot = await characterCollection.get();
+    return snapshot.docs.map((doc) {
+      return CharacterModel.fromMap(doc.data(), doc.id);
+    }).toList();
+  }
 
   Future<UserModel?> getUserProfile(String uid) async {
     try {
@@ -21,4 +32,17 @@ class FirestoreService {
       'nickname': newNickname,
     });
   }
+
+  Future<List<CharacterModel>> getAllCharacters() async {
+    final snapshot = await FirebaseFirestore.instance.collection('characters').get();
+    return snapshot.docs
+        .map((doc) => CharacterModel.fromMap(doc.data(), doc.id))
+        .toList();
+  }
+
+  Future<void> addCharacter(CharacterModel character) async {
+    await FirebaseFirestore.instance.collection('characters').add(character.toMap());
+  }
+
+
 }
